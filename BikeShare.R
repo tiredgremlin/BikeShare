b0e4662b291268cda348c15acf94792b0a289e9c
Liz Baer <ekbaer@byu.edu>
2024-09-11 09:16:15 -0600

    EDA and such

--- BikeShare.R
+++ BikeShare.R
@@ -1,5 +1,4 @@
-library(tidyverse)
-library(tidymodels)
+library(tidyverse); library(tidymodels)
 library(vroom); library(skimr); library(DataExplorer); library(patchwork)
 install.packages()
 
@@ -7,7 +6,7 @@ bike_train <- vroom('bike_train.csv')
 # bike_test <- vroom('bike_test.csv')
 
 DataExplorer::plot_intro(bike_train)
-dplyr::glimpse(bike_train)
+dplyr::glimpse(bike_train1)
 
 bike_train1 <- bike_train %>%
   mutate(season = as.factor(season),
@@ -19,7 +18,7 @@ bike_train1 <- bike_train %>%
 bar_weather <- ggplot(bike_train1, aes(x = weather)) +
   geom_bar()
 
-bar_season <- ggplot(bike_train1, aes(x = season)) +
+bar_work <- ggplot(bike_train1, aes(x = workingday)) +
   geom_bar()
 
 point_temp <- ggplot(bike_train1, aes(x = temp, y = count)) +
@@ -28,4 +27,5 @@ point_temp <- ggplot(bike_train1, aes(x = temp, y = count)) +
 point_humidity <- ggplot(bike_train1, aes(x = humidity, y = count)) +
   geom_point()
 
-(bar_weather + bar_season)/(point_temp + point_humidity)
+(bar_weather + bar_work)/(point_temp + point_humidity)
+
